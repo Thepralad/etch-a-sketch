346c1baa9ef79c3ecdbed16734ce83c75beeafd0
thepralad <prgurung04@gmail.com>
2024-03-08 11:43:46 +0530

    Added  buttons for color selection

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 let grids = [];
+let colorName = ['yellow', 'green', 'blue', 'red', 'orange', 'black'];
+let colorsBtn = [];
+let selectedColor = 'rgb(220, 220, 220)';
 const container = document.querySelector('#container');
 const btn16 = document.querySelector('#one');
 const btn32 = document.querySelector('#two');
 const btn64 = document.querySelector('#three');
+
+for(let i = 0; i < colorName.length; i++){
+    colorsBtn[i] = document.querySelector('#' + colorName[i]);
+}
+colorsBtn.forEach((btn) => {
+    btn.addEventListener('click', () =>{
+        alert(btn.id);
+    })
+})
 
 createGrid(16);
 btn16.addEventListener('click', () => {
